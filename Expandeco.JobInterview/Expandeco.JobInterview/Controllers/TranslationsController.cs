4dd2fb4b9a311f1454cceceafb8a0dcc20a3f970
Polycarp Okock <deaspo@users.noreply.github.com>
2024-01-22 10:36:52 +0100

    TranslationsController - create - Update - Permissions added to returned results for get translatio(s_

--- Expandeco.JobInterview/Expandeco.JobInterview/Controllers/TranslationsController.cs
+++ Expandeco.JobInterview/Expandeco.JobInterview/Controllers/TranslationsController.cs
@@ -4,8 +4,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 
 namespace Expandeco.JobInterview.Controllers
 {
@@ -34,6 +36,7 @@ namespace Expandeco.JobInterview.Controllers
                 .Include(x => x.TargetLanguage)
                 .Include(x => x.CreatedBy)
                 .Include(x => x.AssignedTo)
+                .Include(x => x.Deadline)
                 .ToArray();
             
             switch (loggedUser.TypeId)
@@ -49,7 +52,7 @@ namespace Expandeco.JobInterview.Controllers
             }
         }
 
-        [HttpGet("id")]
+        [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
             var loggedUser = _loggedUserService.Get();
@@ -60,7 +63,8 @@ namespace Expandeco.JobInterview.Controllers
                 .Include(x => x.TargetLanguage)
                 .Include(x => x.CreatedBy)
                 .Include(x => x.AssignedTo)
-                .Where(x => x.AssignedToId ==typeId || x.CreatedById == typeId || typeId == 1)
+                .Include(x => x.Deadline)
+                .Where(x => x.AssignedToId == loggedUser.Id || x.CreatedById == loggedUser.Id || typeId == 1)
                 .FirstOrDefault(x => x.Id == id);
 
             if (result == null)
@@ -68,6 +72,98 @@ namespace Expandeco.JobInterview.Controllers
             
 
             return Ok(result);
+        }
+
+        [HttpPost("create")]
+        [ProducesResponseType(200, Type = typeof(string))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult Create([FromBody] CreateTranslationRequest createTranslationRequest)
+        {
+            createTranslationRequest.Validate();
+            var loggedUser = _loggedUserService.Get();
+            var creator = _dbContext.Users.Include(x => x.Type).FirstOrDefault(x => x.Id == loggedUser.Id);
+            var sourceLanguage = _dbContext.Languages.Find(createTranslationRequest.SourceLanguageId);
+            var targetLanguage = _dbContext.Languages.Find(createTranslationRequest.TargetLanguageId);
+
+            var expiredDeadline = DateTime.Parse(createTranslationRequest.deadline).CompareTo(DateTime.UtcNow) < 0;
+
+            var deadline = new Deadline {
+                DeadlineDateTimeString = DateTime.Parse(createTranslationRequest.deadline, CultureInfo.InvariantCulture).ToString("s"),
+                DeadlineDateTime = DateTime.Parse(createTranslationRequest.deadline, CultureInfo.InvariantCulture),
+                ExpiredDeadline = expiredDeadline
+            };
+
+            var entity = new Translation {
+                Title = createTranslationRequest.Title,
+                Text = createTranslationRequest.Text,
+                SourceLanguageId = sourceLanguage.Id,
+                TargetLanguageId = targetLanguage.Id,
+                CreatedById = loggedUser.Id,
+                CreatedBy = creator,
+                SourceLanguage = sourceLanguage,
+                TargetLanguage = targetLanguage,
+                DeadlineString = deadline.DeadlineDateTime.ToString("f"),
+                Deadline = deadline
+            };
+
+            _dbContext.Translations.Add(entity);
+            _dbContext.SaveChanges();
+
+            return Ok(entity);
+        }
+
+        [HttpPatch("update")]
+        public IActionResult Update([FromBody] UpdateTranslationRequest updateTranslationRequest) {
+            updateTranslationRequest.Validate();
+
+            var loggedUser = _loggedUserService.Get();
+            var typeId = loggedUser.TypeId;
+
+            var translation = _dbContext.Translations.Include(x => x.SourceLanguage)
+                .Include(x => x.TargetLanguage)
+                .Include(x => x.CreatedBy)
+                .Include(x => x.AssignedTo)
+                .FirstOrDefault(x => x.Id == updateTranslationRequest.translationId);
+
+            switch (typeId)
+            {
+                case 1:
+                    // Manager Edits
+                    // Assigned translator
+                    if (updateTranslationRequest.assignedToId != null) {
+                        var assigned = _dbContext.Users.Include(x => x.Type).FirstOrDefault(x => x.Id == updateTranslationRequest.assignedToId);
+
+                        translation.AssignedTo = assigned;
+                        translation.AssignedToId = assigned.Id;
+                    }
+
+                    break;
+                case 2:
+                    // Assigned Edits
+                    // Translated texts
+                    if (!string.IsNullOrEmpty(updateTranslationRequest.Text)) translation.TranslatedText = updateTranslationRequest.translatedText;
+
+                    break;
+                case 3:
+                    // Client Edits
+                    var sourceLanguage = _dbContext.Languages.FirstOrDefault(x => x.Id == updateTranslationRequest.SourceLanguageId);
+                    var targetLanguage = _dbContext.Languages.FirstOrDefault(x => x.Id == updateTranslationRequest.TargetLanguageId);
+                    // Title. Text, Source lang, Target lang
+                    translation.Title = updateTranslationRequest.Title;
+                    translation.Text = updateTranslationRequest.Text;
+                    translation.SourceLanguage = sourceLanguage;
+                    translation.TargetLanguage = targetLanguage;
+                    translation.SourceLanguageId = sourceLanguage.Id;
+                    translation.TargetLanguageId = targetLanguage.Id;
+
+                    break;
+            }
+
+            _dbContext.Translations.Update(translation);
+            _dbContext.SaveChanges();
+
+            return Ok(translation);
         }
     }
 }
